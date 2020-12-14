f5daa2a1b6415b4a42301052206f89aa8b22b8d4
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-12-14 19:00:00 +0000

    post & delete updated

--- src/services/carts/index.js
+++ src/services/carts/index.js
@@ -10,56 +10,67 @@ const {
   getProducts,
   writeProducts,
 } = require("../../lib/utilities");
-const cartsPath = path.join(__dirname,"carts.json");
+const cartsPath = path.join(__dirname, "carts.json");
 
 const { writeFile } = require("fs-extra");
 
-// - You'll be in charge of creating the appropriate endpoint in the BE to store the items your "user" shops in the store. 
+// - You'll be in charge of creating the appropriate endpoint in the BE to store the items your "user" shops in the store.
 
 router.post("/:cartId/add-to-cart/:productId", async (req, res, next) => {
   try {
-    let carts = await readDB(cartsPath)
-    let updatedCart = carts.map(cart => cart._id === req.params.cartId ? {...cart, products:[...cart.products, req.params.productId]} : cart)
-    await writeDB(cartsPath, updatedCart)
-    res.send("ok")
+    let carts = await readDB(cartsPath);
+    let updatedCart = carts.map(cart =>
+      cart._id === req.params.cartId
+        ? { ...cart, products: [...cart.products, req.params.productId] }
+        : cart
+    );
+    await writeDB(cartsPath, updatedCart);
+    res.send("Item added to cart");
   } catch (error) {
     console.log(error);
     next(error);
   }
 });
-  
-// - You need to be able to retrieve those products in the Cart page, therefore creating the endpoint for it in the BE, and display them accordingly. 
+
+// - You need to be able to retrieve those products in the Cart page, therefore creating the endpoint for it in the BE, and display them accordingly.
 router.get("/:cartId", async (req, res, next) => {
   try {
-  let carts = await readDB(cartsPath)
-  let cart  = carts.find(cart => cart._id === req.params.cartId)
-  let products = await getProducts()
-let filteredProducts = products.filter(product => cart.products.findIndex(product._id))
-cart.products = filteredProducts
-res.send(cart)
+    let carts = await readDB(cartsPath);
+    let cart = carts.find(cart => cart._id === req.params.cartId);
+    let products = await getProducts();
+    let filteredProducts = products.filter(product =>
+      cart.products.includes(product._id)
+    );
+    cart.products = filteredProducts;
+    res.send(cart);
   } catch (error) {
     console.log(error);
     next(error);
-    
   }
 });
-
 
 // - Add the functionality to remove items from the Cart.
-router.route("/:cartId/remove-from-cart/:productId").delete(async (req, res, next) => {
-  try {
-    let carts = await readDB(cartsPath)
-    let findCart = carts.find(cart => cart._id === req.params.cartId)
-    if (findCart){
-      let products = await getProducts()
-    let modifiedCart = products.filter(product => product._id != req.params.id)
-    await writeProducts(cartsPath, modifiedCart)
-    res.send("Product has been deleted")
+router
+  .route("/:cartId/remove-from-cart/:productId")
+  .delete(async (req, res, next) => {
+    try {
+      let carts = await readDB(cartsPath);
+      const updatedCart = carts.map(cart =>
+        cart._id === req.params.cartId
+          ? {
+              ...cart,
+              products: cart.products.filter(
+                product => product !== req.params.productId
+              ),
+            }
+          : cart
+      );
+      await writeDB(cartsPath, updatedCart);
+      res.send("Product has been deleted");
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+  });
 
 module.exports = router;
